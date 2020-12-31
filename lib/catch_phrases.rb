e728557c8cfec635a46a164d6c4112cc4b6a51cc
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-12-31 00:37:23 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,26 @@
+
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$phrase = "It's-a me, Mario!"
+$secondphrase = "It's Dangerous To Go Alone! Take This."
+
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  secondphrase =  "It's Dangerous To Go Alone! Take This."
+  puts secondphrase
+end
+
+def catch_phrase(phrase)
+  puts phrase
+end
+  
